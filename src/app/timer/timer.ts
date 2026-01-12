255b4859f038b29a294f9828a1ff26eae94825fc
shalini <s.rathore43@gmail.com>
2026-01-12 15:13:39 +0530

    hk

--- src/app/timer/timer.ts
+++ src/app/timer/timer.ts
@@ -1,17 +1,20 @@
 import { Component ,ChangeDetectorRef } from '@angular/core';
-import { DecimalPipe } from '@angular/common';
+import { DecimalPipe, NgSwitch, NgSwitchCase } from '@angular/common';
 import { interval } from 'rxjs';
+import { CommonModule } from '@angular/common';
 @Component({
   selector: 'app-timer',
-  imports: [DecimalPipe],
+  imports: [DecimalPipe,CommonModule, NgSwitch, NgSwitchCase],
   templateUrl: './timer.html',
   styleUrl: './timer.css',
 })
 export class Timer {
   minutes:number;
   seconds:number;
+  timeLeftColor: string;
   constructor(private cdr: ChangeDetectorRef) {
     this.minutes = 3;
+     this.timeLeftColor = 'haveTime';
     this.seconds = 59;
     // setInterval(() => this.tick(), 1000);
    }
@@ -21,12 +24,22 @@ export class Timer {
    }
   tick() {
     this.cdr.detectChanges();
-    if (--this.seconds < 0) {
+    console.log(this.seconds)
+    if (--this.seconds < 0) {     
       this.seconds = 59;
       if (--this.minutes < 0) {
         this.minutes = 3;
         this.seconds = 59;
       }
     }
+     if (this.minutes >= 2) {
+      this.timeLeftColor = 'haveTime';
+      return;
+    }
+    if (this.minutes < 2 && this.minutes >= 1) {
+      this.timeLeftColor = 'lessTime';
+      return;
+    }
+    this.timeLeftColor = 'noTime';
   } 
 }
